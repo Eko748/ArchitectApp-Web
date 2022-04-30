bfd4530373da3d6fcd0fe5727eae23e144966e6e
Eko Permana <permanaeko@gmail.com>
2022-04-30 08:10:52 +0700

    UI Lelang Konsultan dan SideBar proses

--- resources/views/layouts/sidebar-konsultan.blade.php
+++ resources/views/layouts/sidebar-konsultan.blade.php
@@ -9,24 +9,25 @@
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li class="{{ Request::segment(2) == 'dashboard' ? 'active' : '' }}"><a class="nav-link"
-                    href="{{ route('konsultan.dashboard') }}"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
+                    href="{{ route('konsultan.dashboard') }}"><i class="fas fa-fire"></i><span>Home</span></a></li>
             <li class="menu-header">Product</li>
-            <li class=" {{ Request::segment(2) == 'project' ? 'active' : '' }}"><a class="nav-link"  href="{{ route('konsultan.project') }}"><i class="far fa-images"></i><span>Project</span></a></li>
+            <li class=" {{ Request::segment(2) == 'project' ? 'active' : '' }}"><a class="nav-link"  href="{{ route('konsultan.project') }}"><i class="far fa-images"></i><span>Desain Saya</span></a></li>
+            <li class=" {{ Request::segment(2) == 'lelang-konsultan' ? 'active' : '' }}"><a class="nav-link"  href="{{ route('konsultan.lelang-konsultan') }}"><i class="far fa-images"></i><span>Lelang Saya</span></a></li>
             <li class="menu-header">Management</li>
-            <li class=" {{ Request::segment(2) == 'lelang' ? 'active' : '' }}"><a class="nav-link"  href="{{ route('konsultan.find') }}"><i class="far fa-images"></i><span>Find Work</span></a></li>
+            <li class=" {{ Request::segment(2) == 'lelang' ? 'active' : '' }}"><a class="nav-link"  href="{{ route('konsultan.lelang.owner') }}"><i class="far fa-images"></i><span>Lelang Owner</span></a></li>
             <li class="nav-item dropdown  {{ in_array(Request::segment(3),['active-job','archived-job']) ? 'active' : '' }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                        class="fas fa-users"></i><span>My Jobs</span></a> </a>
+                        class="fas fa-users"></i><span>Project</span></a> </a>
                 <ul class="dropdown-menu">
                     <li class="{{ Request::segment(3) == 'active-job' ? 'active' : '' }}"><a class="nav-link"
-                            href="{{ route('konsultan.job.active') }}">Active Jobs</a></li>
+                            href="{{ route('konsultan.job.active') }}">Active Project</a></li>
                     <li class="{{ Request::segment(3) == 'archived-job' ? 'active' : '' }}"><a class="nav-link"
-                            href="{{ route('konsultan.job.archived') }}">Archived Jobs</a></li>
+                            href="{{ route('konsultan.job.archived') }}">Archived Project</a></li>
                 </ul>
             </li>
             <li class="nav-item dropdown  {{ in_array(Request::segment(3),['active','submit','archived']) ? 'active' : '' }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                        class="fas fa-users"></i><span>My Proposal</span></a> </a>
+                        class="fas fa-users"></i><span>Proposal</span></a> </a>
                 <ul class="dropdown-menu">
                     <li class="{{ Request::segment(3) == 'active' ? 'active' : '' }}"><a class="nav-link"
                             href="{{ route('konsultan.proposal.active') }}">Active Proposal</a></li>
