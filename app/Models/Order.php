bb976204bfa39f844bfdca1e1471bbaeb673f179
Eko Permana <permanaeko@gmail.com>
2022-05-19 21:38:56 +0700

    Model & Migration Order

---
+++ app/Models/Order.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    // protected $fillable = [
+    //     'kontrakKonsultanId', 'buktiBayar','status'
+    // ];
+    protected $guarded = [];
+
+    public function kontrak()
+    {
+        return $this->belongsTo(KontrakKerjaKonsultan::class, 'kontrakKonsultanId','id');
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class, 'ownerId', 'id');
+    }
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'projectId', 'id');
+    }
+}
---
+++ database/migrations/2022_05_19_113838_create_orders_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('kontrakKonsultanId');
+            $table->integer('ownerId');
+            $table->integer('projectId');
+            // $table->text('buktiBayar');
+            // $table->integer('status');
+            $table->integer('status');
+            $table->string('status_order');
+            $table->string('transaction_id');
+            $table->string('order_id');
+            $table->string('gross_amount');
+            $table->string('payment_type');
+            $table->string('payment_code')->nullable();
+            $table->string('pdf_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
